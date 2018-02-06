a3258f2cf3ae2928b7252bc9eaf1471bd79222ac
Jschles1 <jschles1@ramapo.edu>
2018-02-05 22:21:12 -0500

    Created Navbar component, modified styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
-    
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
+
 export default {
-  
+  components: {
+    'navbar': Navbar
+  }
 }
 </script>
 
 <style lang="scss">
-
+  #app {
+    width: 95%;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="#signin">Sign in</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  nav {
+    background-color: white;
+    color: skyblue;
+  }
+
+  nav ul li {
+    list-style: none;
+    text-decoration: none;
+    float: right;
+  }
+</style>
