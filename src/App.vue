7ab74bb4999a42e12f31fe763da6f62693fb527f
Jschles1 <jschles1@ramapo.edu>
2018-02-09 23:29:11 -0500

    Added material icons, Mission component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <navbar></navbar>
     <intro></intro>
+    <mission></mission>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import Intro from './components/Intro.vue';
+import Mission from './components/Mission.vue';
 
 export default {
   components: {
     'navbar': Navbar,
-    'intro': Intro
+    'intro': Intro,
+    'mission': Mission
   }
 }
 </script>
---
+++ src/components/Mission.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mission-container">
+    <div class="mission-content">
+      <h1>Unlock Your Greatest Potential With The Right Resources</h1>
+      <p>
+        Fownders was built out of a necessity for aspiring entrepreneurs to have a place to develop the hard, tactical skills needed to succeed. The skills that are not taught in school.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  .mission-container {
+    background-color: white;
+    padding: 30px 0;
+  }
+
+  .mission-content {
+    width: 80%;
+    margin: 0 auto;
+    color: skyblue;
+    text-align: center;
+
+    p {
+      color: black;
+    }
+  }
+</style>
