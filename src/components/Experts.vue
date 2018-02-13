4575d7eee5e0951ce32f342a0ecbc5c67571ac6c
Jschles1 <jschles1@ramapo.edu>
2018-02-12 22:23:55 -0500

    Added box placeholders for images

--- src/components/Experts.vue
+++ src/components/Experts.vue
@@ -3,9 +3,37 @@
     <div class="experts-content">
       <h1 class="experts-main-header">Learn from THE Experts...</h1>
       <h1>the ones you wish you knew when you started!</h1>
+
       <p>
         Fownders partners with legendary experts across a variety of fields. They share their knowledge and experience with you in their lessons, Masterclasses, and workshops. Here are just a few lessons you can start watching today:
       </p>
+
+      <div class="expert-row">
+        <div class="expert-box">
+
+        </div>
+        <div class="expert-box">
+          
+        </div>
+      </div>
+
+      <div class="expert-row">
+        <div class="expert-box">
+
+        </div>
+        <div class="expert-box">
+          
+        </div>
+      </div>
+
+      <div class="expert-row">
+        <div class="expert-box">
+
+        </div>
+        <div class="expert-box">
+          
+        </div>
+      </div>
       <button class="join">JOIN FOWNDERS</button>
     </div>
   </div>
@@ -30,6 +58,17 @@ export default {
 
       .experts-main-header {
         color: skyblue;
+      }
+
+      .expert-row {
+        
+        .expert-box {
+          display: inline-block;
+          height: 250px;
+          width: 400px;
+          background-color: white;
+          margin: 20px 10px;
+        }
       }
     }
   }
