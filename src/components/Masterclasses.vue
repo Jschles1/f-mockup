a3e21d7290bc7aaeee82c3144f3945c843f74411
Jschles1 <jschles1@ramapo.edu>
2018-02-13 22:07:40 -0500

    Added content to Masterclasses

--- src/components/Masterclasses.vue
+++ src/components/Masterclasses.vue
@@ -5,6 +5,73 @@
       <p>
         In addition to our mini lessons and workshops available within the app, you also get access to all of our Masterclasses. Masterclasses help you achieve one specific result and often contain a whole series of video lessons to teach you what you need to know from A-Z on a particular topic. New Masterclasses are being added every month - be sure to let us know in our Slack Channel which Masterclasses you would like to see shortly. Here are some of the Masterclasses you can start watching today:
       </p>
+
+      <div class="master-row">
+        <div class="master-box">
+
+        </div>
+        <div class="master-box">
+          <p>
+            30+ videos about unlocking your potential, developing your personal brand, growing your following on social media, and building real relationships. Use the power of Social Currency to become a prominent influencer and build a thriving personal brand all while living your purpose.
+          </p>
+        </div>
+      </div>
+
+      <div class="master-row">
+        <div class="master-box">
+
+        </div>
+        <div class="master-box">
+          <p>
+            Overcome fear and become who you are capable of becoming. Learn how to break free of your limiting beliefs and get rid of all your fears with the new Mindset Masterclass.
+          </p>
+        </div>
+      </div>
+
+      <div class="master-row">
+        <div class="master-box">
+
+        </div>
+        <div class="master-box">
+          <p>
+            Learn how to establish a mission, vision, recruit an early stage team, and bring your idea to reality in a big way with Adam Braun.
+          </p>
+        </div>
+      </div>
+
+      <div class="master-row">
+        <div class="master-box">
+
+        </div>
+        <div class="master-box">
+          <p>
+            In this masterclass, learn how you can start your very OWN clothing brand that’ll make it in the industry.
+          </p>
+        </div>
+      </div>
+
+      <div class="master-row">
+        <div class="master-box">
+
+        </div>
+        <div class="master-box">
+          <p>
+            In this Cryptocurrency Masterclass, you’ll learn how cryptocurrency works, how you can build a sustainable organization, and launch an ICO.
+          </p>
+        </div>
+      </div>
+
+      <div class="master-row">
+        <div class="master-box">
+
+        </div>
+        <div class="master-box">
+          <p>
+            So you want to start making some money by selling things on Shopify, but you have no idea where to start, right? Emilio D’Angelo is a Shopify guru who took his business to a million dollars in six months and is going to teach you exactly how he did it.
+          </p>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -25,6 +92,20 @@ export default {
       margin: 0 auto;
       color: white;
       text-align: center;
+
+      .master-row {
+        vertical-align: top;
+        
+        .master-box {
+          overflow: hidden;
+          display: inline-block;
+          height: 250px;
+          width: 400px;
+          border: 1px solid white;
+          margin: 20px 10px;
+          text-align: left;
+        }
+      }
     }
   }
 
