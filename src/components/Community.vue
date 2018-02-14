89982b57f4408d433482d112d648f5bbff01842e
Jschles1 <jschles1@ramapo.edu>
2018-02-13 21:51:57 -0500

    Added content to Community

--- src/components/Community.vue
+++ src/components/Community.vue
@@ -2,6 +2,60 @@
   <div class="comm-container">
     <div class="comm-content">
       <h1>Advance Your Business & Your Lifestyle</h1>
+      <p>
+        Join a global community of like-minded entrepreneurs, creatives, and visionaries. Be inspired to think bigger, contribute back, and receive support while you express your purpose.
+      </p>
+      <div class="comm-row">
+        <div class="comm-box">
+
+        </div>
+        <div class="comm-box">
+          <h3>SLACK COMMUNITY</h3>
+          <p>
+            Our Premium Private Slack Channel is the place for you to interact and build real relationships with other entrepreneurs around the world. Within this select group of entrepreneurs you will find mentors, leaders, and business owners from all over the globe.
+          </p>
+        </div>
+      </div>
+
+      <div class="comm-row">
+        <div class="comm-box">
+          <h3>FACEBOOK LIVESTREAMS</h3>
+          <p>
+            Our leadership team and mentors will answer your questions on personal and business development, tactical marketing strategies, and more within the exclusive Facebook community via a private Facebook live stream every month.
+          </p>
+        </div>
+        <div class="comm-box">
+          
+        </div>
+      </div>
+
+      <div class="comm-row">
+        <div class="comm-box">
+          
+        </div>
+        <div class="comm-box">
+          <h3>DOWNLOADABLE RESOURCES</h3>
+          <p>
+            Download secrets of the trade! Each Masterclass includes checklists that are significant assets to use to your advantage.
+          </p>
+        </div>
+      </div>
+
+      <div class="comm-row">
+        <div class="comm-box">
+          <h3>OFFLINE EXPERIENCES</h3>
+          <p>
+            What you get is priority access and discounts to our best events, experiences, and gatherings. Network at our startup grinds, workshops, masterminds, and summits. And most importantly: build real personal connections with amazing people that care about providing you value.
+          </p>
+        </div>
+        <div class="comm-box">
+          
+        </div>
+      </div>
+
+      <br>
+
+      <button class="join">JOIN FOWNDERS</button>
     </div>
   </div>
 </template>
@@ -22,6 +76,22 @@ export default {
       margin: 0 auto;
       color: white;
       text-align: center;
+      
+
+      .comm-row {
+        display: inline-block;
+        vertical-align: top;
+        
+        .comm-box {
+          overflow: hidden;
+          display: inline-block;
+          height: 210px;
+          width: 350px;
+          border: 1px solid gray; 
+          margin: 20px 5px;
+          text-align: left;
+        }
+      }
     }
   }
 </style>
